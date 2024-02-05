5a32c511284afa93527d76ba1e8a7b258b6aed54
Valentine Orga <valentineorga@gmail.com>
2024-02-05 12:43:36 +0100

    Search page

--- src/screens/Home/Tab/Search.tsx
+++ src/screens/Home/Tab/Search.tsx
@@ -1,10 +1,28 @@
-import { View } from 'native-base'
+import { ScrollView, Text, VStack, View } from 'native-base'
 import React from 'react'
+import { SearchBar } from '@rneui/themed'
+import Post from '../../../components/Post'
+
 
 type Props = {}
 
+const categories = ["Sports", "Lifestyle", "Tech", "Art", "Literature", "Poetry"]
+
 export default function Search({ }: Props) {
     return (
-        <View></View>
+        <VStack bgColor="white" flex="1" px='4' py='2' space="5">
+            <SearchBar platform='android' placeholder='What are you looking for?' containerStyle={{ borderWidth: 1, borderRadius: 30 }} />
+            <View flexDir="row" justifyContent="space-beween" alignItems="center">
+                {categories.map(category => (
+                    <View key={category} borderWidth={1} borderRadius={10} px={4} py={2}>
+                        <Text>{category}</Text>
+                    </View>
+                ))}
+            </View>
+
+            <ScrollView>
+                {categories.map((category, index) => <Post key={category} content={category} live={index % 2 == 0} audio={index % 2 != 0} />)}
+            </ScrollView>
+        </VStack>
     )
 }
